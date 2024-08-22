7a72cdb1849ddf0c1849091a48d5c027cdaf7ba4
Virtue8 <virtue_8@mail.ru>
2024-08-22 13:57:06 +0300

    Added structures and 9 tests

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -3,6 +3,10 @@
 #include <math.h>
 #include <assert.h>
 
+#define RESET   "\033[0m"
+#define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+
 // #define DEBUG
 
 enum roots
@@ -29,7 +33,15 @@ void CleanBuffer();
 bool IsZero(double eps);
 bool CompareTwo(double k1, double k2);
 
-int UnitTester (double a, double b, double c, double x1_exp, double x2_exp, int nRoots_exp);
+struct UnitSample
+{
+    double a, b, c;
+    double x1_exp, x2_exp;
+    int nRoots_exp;
+    int TestNumber;
+};
+
+int UnitTester(struct UnitSample test);
 void AllUnitsTester();
 
 //-----------------------------------------------------------------------
@@ -46,9 +58,9 @@ int main()
     double x2 = 0;
 
     Intro();
-    Input(&a, &b, &c);
-
     printf("**Manual Mode**\n\n");
+
+    Input(&a, &b, &c);
 
     int nRoots = Dispatcher(a, b, c, &x1, &x2);
     Output(nRoots, x1, x2);
@@ -118,20 +130,20 @@ int Dispatcher(double a, double b, double c, double *x1, double *x2)
 
     if (IsZero(a))
     {
-        printf("Your equation is linear (b*x + c = 0).\n");
+        //printf("Your equation is linear (b*x + c = 0).\n");
         return SolveLinear(b, c, x1);
     }
     else if (IsZero(a) == false && IsZero(b) == false && IsZero(c))
     {
-        *x2 = 0;
-        printf("Your equation is semi-square (a*x^2 + b*x = 0).\n"
-               "One of the roots is x = %lg.\n"
-               "Now the equation is linear.\n", x2);
+        x2 = 0;
+        //printf("Your equation is semi-square (a*x^2 + b*x = 0).\n"
+          //     "One of the roots is x = %lg.\n"
+            //   "Now the equation is linear.\n", *x2);
         return SolveLinear(a, b, x1);
     }
     else
     {
-        printf("Your equation is square (a*x^2 + b*x + c = 0).\n");
+        //printf("Your equation is square (a*x^2 + b*x + c = 0).\n");
         return SolveSquare(a, b, c, x1, x2);
     }
 }
@@ -250,65 +262,59 @@ void CleanBuffer()
 
 void AllUnitsTester()
 {
+    UnitSample test1 = {1, 2, -3, -3, 1, TWO_ROOTS, 1};
+    UnitSample test2 = {1, -5, 4, 5, 1, TWO_ROOTS, 2};
+    UnitSample test3 = {1, -2, 1, 1, 1, ONE_ROOT, 3};
+    UnitSample test4 = {1, 25, 0, -25, 0, ONE_ROOT, 4};
+    UnitSample test5 = {0, -2, 1, 0.5, 0, ONE_ROOT, 5};
+    UnitSample test6 = {3, 4, 5, 0, 0, NO_ROOTS, 6};
+    UnitSample test7 = {1, 2.5, -1.5, -3, 0.5, TWO_ROOTS, 7};
+    UnitSample test8 = {0, 0, 3, 0, 0, NO_ROOTS, 8};
+    UnitSample test9 = {0, 0, 0, 0, 0, INF_ROOTS, 9};
+
+
     int counter = 0;
 
-    double x1_exp = -3;
-    double x2_exp = 1;
-    int nRoots_exp = 2;
+    counter += UnitTester(UnitSample(test1));
+    counter += UnitTester(UnitSample(test2));
+    counter += UnitTester(UnitSample(test3));
+    counter += UnitTester(UnitSample(test4));
+    counter += UnitTester(UnitSample(test5));
+    counter += UnitTester(UnitSample(test6));
+    counter += UnitTester(UnitSample(test7));
+    counter += UnitTester(UnitSample(test8));
+    counter += UnitTester(UnitSample(test9));
 
-    double a = 1;
-    double b = 2;
-    double c = -3;
-
-    counter += UnitTester(a, b, c, x1_exp, x2_exp, nRoots_exp);
-
-    x1_exp = 5;
-    x2_exp = 1;
-    nRoots_exp = 2;
-
-    a = 1;
-    b = -5;
-    c = 4;
-
-    counter += UnitTester(a, b, c, x1_exp, x2_exp, nRoots_exp);
-
-    x1_exp = 1;
-    x2_exp = 1;
-    nRoots_exp = 1;
-
-    a = 1;
-    b = -2;
-    c = 1;
-
-    counter += UnitTester(a, b, c, x1_exp, x2_exp, nRoots_exp);
-
-    printf("Total: %d correct, %d incorrect.\n", counter, 3 - counter);
+    printf("Total: %d correct, %d incorrect.\n", counter, 9 - counter);
 }
 
 //------------------------------------------------------------------------------
 
-int UnitTester (double a, double b, double c, double x1_exp, double x2_exp, int nRoots_exp)
+int UnitTester(struct UnitSample test)
 {
-    assert(isfinite(a));
-    assert(isfinite(b));
-    assert(isfinite(c));
-    assert(isfinite(x1_exp));
-    assert(isfinite(x2_exp));
+    assert(isfinite(test.a));
+    assert(isfinite(test.b));
+    assert(isfinite(test.c));
+    assert(isfinite(test.x1_exp));
+    assert(isfinite(test.x2_exp));
 
     double x1 = 0;
     double x2 = 0;
 
-    int nRoots = SolveSquare(a, b, c, &x1, &x2);
+    int nRoots = Dispatcher(test.a, test.b, test.c, &x1, &x2);
 
-    if (CompareTwo(nRoots, nRoots_exp) == false || CompareTwo(x1, x1_exp) == false || CompareTwo(x2, x2_exp) == false)
+    if (CompareTwo(nRoots, test.nRoots_exp) == false || CompareTwo(x1, test.x1_exp) == false || CompareTwo(x2, test.x2_exp) == false)
     {
-        printf("-- Incorrect execution: a = %lg, b = %lg, c = %lg, x1 = %lg, x2 = %lg, nRoots = %d\n"
-               "Expected: x1 = %lg, x2 = %lg, nRoots = %d.\n\n", a, b, c, x1, x2, nRoots, x1_exp, x2_exp, nRoots_exp);
+        printf("%sTest #%d : Incorrect execution:%s a = %lg, b = %lg, c = %lg, "
+               "x1 = %lg, x2 = %lg, nRoots = %d\n"
+               "Expected: x1 = %lg, x2 = %lg, nRoots = %d.\n\n",
+               RED, test.TestNumber, RESET, test.a, test.b, test.c, x1, x2,
+               nRoots, test.x1_exp, test.x2_exp, test.nRoots_exp);
         return 0;
     }
     else
     {
-        printf("++ Correct execution.\n\n");
+        printf("%sTest #%d : Correct execution.%s\n\n", GREEN, test.TestNumber, RESET);
         return 1;
     }
 }
