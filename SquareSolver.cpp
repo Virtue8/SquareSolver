db05d27987ed364bda785229628efa4dfe9b3aeb
Virtue8 <virtue_8@mail.ru>
2024-08-24 12:38:52 +0300

    V2 finale (added structures, arrays, mode switcher updated)

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -8,11 +8,11 @@
 #define GREEN   "\033[1;32m"
 
 enum modes
-{
-    MANUAL = 'm',
-    TEST = 't'
-};
-
+{                                                //    To-Do list for 23.08
+    MANUAL = 'm',                                //    0) Mode Switching
+    TEST = 't'                                   //    1) struct refactoring(entering, initialization, etc.)
+};                                               //    2) file splitting
+                                                 //    3) doxygen
 enum roots
 {
     NO_ROOTS,
@@ -21,20 +21,7 @@ enum roots
     INF_ROOTS
 };
 
-struct UnitSample
-{
-    double a;
-    double b;
-    double c;
-
-    double x1_exp;
-    double x2_exp;
-
-    int nRoots_exp;
-    int TestNumber;
-};
-
-struct Coeff
+struct Coefficients
 {
     double a;
     double b;
@@ -45,26 +32,39 @@ struct Roots
 {
     double x1;
     double x2;
+    int nRoots;
 };
 
-// files
-// doxygen
-// struct for coefficients for roots
+struct UnitSample
+{
+    struct Coefficients coeff;
+    struct Roots roots_exp;
+    struct Roots roots;
+
+    int TestNumber;
+};
+
+// раздельная компиляция         // перед понедельником
+// files                         // перед понедельником
+// doxygen                       // сразу
+// аргументы командной строки    // сразу (argc, argv)
+// array struct
 
 const double EPSILON = 1e-10;
 
-int SolveLinear (double b, double c, double* x1);
-int SolveSquare (double a, double b, double c, double* x1, double* x2);
-
-int Dispatcher(double a, double b, double c, double *x1, double *x2, char ModeState);
+int SolveLinear (struct Coefficients coeff, struct Roots *roots);
+int SolveSemiLinear (struct Coefficients coeff, struct Roots *roots);
+int SolveSquare (struct Coefficients coeff, struct Roots *roots);
+int Dispatcher(struct Coefficients coeff, struct Roots *roots);
 
 void Menu ();
 void ManualMode ();
 void TestMode ();
-void ModeSwitcher(char *ModeState);
+void ModeSwitcher ();
+char GetMode ();
 
-void Input (double* a, double* b, double* c);
-void Output (int nRoots, double x1, double x2);
+void Input (struct Coefficients *coeff);
+void Output (struct Roots roots);
 
 void CleanBuffer ();
 
@@ -72,9 +72,6 @@ bool IsZero (double eps);
 bool CompareTwo (double k1, double k2);
 
 int UnitTester (struct UnitSample test);
-void AllUnitsTester ();
-
-char ModeState = 0;
 
 //-----------------------------------------------------------------------
 
@@ -89,13 +86,13 @@ int main ()
 
 void Menu ()
 {
-    printf ("#######################\n"
-            "#####     ######     ##\n"
-            "####   ########   #####\n"
-            "#####   ########   ####\n"
-            "######   ########   ###\n"
-            "###     ######     ####\n"
-            "#######################\n\n");
+    printf ("#####################\n"
+            "###     ####      ###\n"
+            "###  ##  #####  #####\n"
+            "###     ######  #####\n"
+            "###  #  ######  #####\n"
+            "###  ##  #####  #####\n"
+            "#####################\n\n");
 
     printf ("SQUARE EQUATION SOLVER (V1) \n"
             "Dolgopa City, 2024 \n");
@@ -105,28 +102,51 @@ void Menu ()
             "m for Manual Mode\n"
             "t for Test Mode\n");
 
-    ModeSwitcher(&ModeState);
+    ModeSwitcher ();
 }
 
 //-----------------------------------------------------------------------
 
-void ModeSwitcher(char *ModeState)
+void ModeSwitcher ()
 {
-    char mode;
-    scanf ("%c", &mode);
-
-    switch (mode)
+    switch (GetMode () )
     {
         case MANUAL : ManualMode ();
-                      *ModeState = MANUAL;
                       break;
 
         case TEST   : TestMode ();
                       break;
 
-        default     : CleanBuffer ();
-                      printf("Wrong format of input, try again, i believe in you.\n");
-                      ModeSwitcher(ModeState);
+        default     : printf("Wrong format of input, try again, i believe in you.\n");
+                      ModeSwitcher ();
+    }
+}
+
+//-----------------------------------------------------------------------
+
+char GetMode ()
+{
+    while (true)
+    {
+        int ch1 = getchar ();
+        int ch2 = getchar ();
+
+        if (ch2 == '\n')
+        {
+            if (ch1 == MANUAL)
+            {
+                return MANUAL;
+            }
+            else if (ch1 == TEST)
+            {
+                return TEST;
+            }
+        }
+        else
+        {
+            CleanBuffer ();
+            return 0;
+        }
     }
 }
 
@@ -134,100 +154,153 @@ void ModeSwitcher(char *ModeState)
 
 void ManualMode ()
 {
-    double a = 0;
-    double b = 0;
-    double c = 0;
+    Coefficients coeff = {};
+    Roots roots = {};
 
-    double x1 = 0;
-    double x2 = 0;
+    printf ("\n**Manual Mode**\n\n");
 
-    printf ("**Manual Mode**\n\n");
-
-    Input (&a, &b, &c);
-
-    int nRoots = Dispatcher (a, b, c, &x1, &x2, ModeState);
-    Output (nRoots, x1, x2);
+    Input (&coeff);
+    roots.nRoots = Dispatcher (coeff, &roots);
+    Output (roots);
 }
 
 //-----------------------------------------------------------------------
 
 void TestMode ()
 {
-    printf ("**Test mode (Unit Testing)**\n\n");
-    AllUnitsTester ();
+    printf ("\n**Test mode (Unit Testing)**\n\n");
+    const int nTests = 9;
+    int counter = 0;
+
+    UnitSample test[nTests] = {//     a     b     c     x1_exp   x2_exp   nRoots_exp    x1    x2    nRoots       TestNumber
+                                    {{1,    2,   -3},   {-3,     1,       TWO_ROOTS},   {0,   0,    NO_ROOTS},     1},
+                                    {{1,    -5,  4},    { 1,     4,       TWO_ROOTS},   {0,   0,    NO_ROOTS},     2},
+                                    {{1,    -2,  1},    { 1,     1,       ONE_ROOT},    {0,   0,    NO_ROOTS},     3},
+                                    {{1,    25,  0},    {-25,    0,       TWO_ROOTS},   {0,   0,    NO_ROOTS},     4},
+                                    {{0,    -2,  1},    { 0.5,   0,       ONE_ROOT},    {0,   0,    NO_ROOTS},     5},
+                                    {{3,    4,   5},    { 0,     0,       NO_ROOTS},    {0,   0,    NO_ROOTS},     6},
+                                    {{1,    3.5, 2.5},  {-2.5,  -1,       TWO_ROOTS},   {0,   0,    NO_ROOTS},     7},
+                                    {{0,    0,   8},    { 0,     0,       NO_ROOTS},    {0,   0,    NO_ROOTS},     8},
+                                    {{0,    0,   0},    { 0,     0,       INF_ROOTS},   {0,   0,    NO_ROOTS},     9}};
+
+    for (int i = 0; i < nTests; i++)
+    {
+        test[i].roots.nRoots = Dispatcher(test[i].coeff, &test[i].roots);
+        counter += UnitTester(test[i]);
+    }
+
+    printf("\n--------------------------------\n"
+             "Total: %d correct, %d incorrect.\n"
+             "--------------------------------", counter, nTests - counter);
 }
+
+//------------------------------------------------------------------------------
+
+int UnitTester(struct UnitSample test)
+{
+    //asserts are temporary deleted
+
+    if (test.roots_exp.nRoots != test.roots.nRoots || CompareTwo (test.roots_exp.x1, test.roots.x1) == false || CompareTwo (test.roots.x2, test.roots_exp.x2) == false)
+    {
+        printf ("%s----------\n"
+               "|Test #%d |: Incorrect execution:%s a = %lg, b = %lg, c = %lg,\n"
+                "%s----------%s                       x1 = %lg, x2 = %lg, nRoots = %d\n\n"
+                "            Expected:            x1 = %lg, x2 = %lg, nRoots = %d.\n\n",
+                RED, test.TestNumber, RESET, test.coeff.a, test.coeff.b, test.coeff.c, RED, RESET, test.roots.x1, test.roots.x2,
+                test.roots.nRoots, test.roots_exp.x1, test.roots_exp.x2, test.roots_exp.nRoots);
+        return 0;
+    }
+    else
+    {
+        printf ("%s--------- \n"
+                "|Test #%d | : Correct execution!\n"
+                "--------- %s\n\n", GREEN, test.TestNumber, RESET);
+        return 1;
+    }
+    return 0;
+}
+
 
 //-----------------------------------------------------------------------
 
-void Input (double* a, double* b, double* c)
+void Input (struct Coefficients *coeff)
 {
-    assert (a != NULL);
-    assert (b != NULL);
-    assert (c != NULL);
-    assert (a != b && a != c && b != c);
+    assert (coeff != NULL);
+
+    assert (isfinite (coeff->a) );
+    assert (isfinite (coeff->b) );
+    assert (isfinite (coeff->c) );
 
     printf ("Enter coefficients for ax^2+bx+c=0 equation ");
     printf ("in the following format: a b c \n \n");
 
-    while (scanf ("%lg %lg %lg", a, b, c) != 3)
+
+    while (true)
     {
-        CleanBuffer ();
-        printf("Wrong format of input, try again, i believe in you.\n");
+        if (scanf ("%lg %lg %lg", &coeff->a, &coeff->b, &coeff->c) == 3)
+        {
+            if (getchar() != '\n')
+            {
+                printf ("Wrong format of input, try again, i believe in you.\n");
+                CleanBuffer ();
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            printf ("Wrong format of input, try again, i believe in you.\n");
+            CleanBuffer ();
+        }
     }
 }
 
 //-----------------------------------------------------------------------
 
-int Dispatcher (double a, double b, double c, double *x1, double *x2, char ModeState)
+int Dispatcher (struct Coefficients coeff, struct Roots *roots)
 {
-    assert (x1 != x2);
-    assert (x2 != NULL);
-    assert (x1 != NULL);
+    assert (roots != NULL);
 
-    assert (isfinite (a) );
-    assert (isfinite (b) );
-    assert (isfinite (c) );
+    assert (isfinite (coeff.a) );
+    assert (isfinite (coeff.b) );
+    assert (isfinite (coeff.c) );
 
-    if (IsZero (a) )
+    if (IsZero (coeff.a) )
     {
-        if (ModeState = MANUAL)
-        {
-        printf ("Your equation is linear (b*x + c = 0).\n");
-        }
-        return SolveLinear (b, c, x1);
+        return SolveLinear (coeff, roots);
     }
-    else if (IsZero (c) )
+    else if (IsZero (coeff.c) )
     {
-        x2 = 0;
-        if (ModeState = MANUAL)
-        {
-        printf ("Your equation is semi-square (a*x^2 + b*x = 0).\n"
-                "One of the roots is x = %lg.\n"
-                "Now the equation is linear.\n", *x2);
-        }
-        return SolveLinear (a, b, x1);
+        return SolveSemiLinear (coeff, roots);
     }
     else
     {
-        if (ModeState = MANUAL)
-        {
-        printf ("Your equation is square (a*x^2 + b*x + c = 0).\n");
-        }
-        return SolveSquare (a, b, c, x1, x2);
+        return SolveSquare (coeff, roots);
     }
+    return 0;
 }
 
 //-----------------------------------------------------------------------
 
-int SolveLinear (double b, double c, double* x1)
+int SolveSemiLinear(struct Coefficients coeff, struct Roots *roots)
 {
-    assert (x1 != NULL);
-    assert (isfinite (b) );
-    assert (isfinite (c) );
+    roots->x1 = -coeff.b / coeff.a;
+    return TWO_ROOTS;
+}
 
-    if (IsZero (b) )
+//-----------------------------------------------------------------------
+
+int SolveLinear (struct Coefficients coeff, struct Roots *roots)
+{
+    assert (roots != NULL);
+    assert (isfinite (coeff.b) );
+    assert (isfinite (coeff.c) );
+
+    if (IsZero (coeff.b) )
     {
-        if (IsZero (c) )
+        if (IsZero (coeff.c) )
         {
             return INF_ROOTS;
         }
@@ -238,24 +311,24 @@ int SolveLinear (double b, double c, double* x1)
     }
     else
     {
-        *x1 = -c / b;
+        roots->x1 = -coeff.c / coeff.b;
         return ONE_ROOT;
     }
 }
 
 //-----------------------------------------------------------------------
 
-int SolveSquare (double a, double b, double c, double* x1, double* x2)
+int SolveSquare (struct Coefficients coeff, struct Roots *roots)
 {
-    assert (x1 != x2);
-    assert (x2 != NULL);
-    assert (x1 != NULL);
+    assert (roots != NULL);
+    assert (&coeff != NULL);
+    assert (isfinite (roots->x1) );
+    assert (isfinite (roots->x2) );
+    assert (isfinite (coeff.a) );
+    assert (isfinite (coeff.b) );
+    assert (isfinite (coeff.c) );
 
-    assert (isfinite (a) );
-    assert (isfinite (b) );
-    assert (isfinite (c) );
-
-    double Discriminant = b * b - 4 * a * c;
+    double Discriminant = coeff.b * coeff.b - 4 * coeff.a * coeff.c;
 
     if (Discriminant < -EPSILON)
     {
@@ -265,8 +338,8 @@ int SolveSquare (double a, double b, double c, double* x1, double* x2)
     {
         Discriminant = sqrt(Discriminant);
 
-        *x1 = (-b - Discriminant) / (2 * a);
-        *x2 = (-b + Discriminant) / (2 * a);
+        roots->x1 = (-coeff.b - Discriminant) / (2 * coeff.a);
+        roots->x2 = (-coeff.b + Discriminant) / (2 * coeff.a);
 
         if (IsZero (Discriminant) == true)
         {
@@ -281,30 +354,30 @@ int SolveSquare (double a, double b, double c, double* x1, double* x2)
 
 //-----------------------------------------------------------------------
 
-void Output (int nRoots, double x1, double x2)
+void Output (struct Roots roots)
 {
-    assert (isfinite (x1) );
-    assert (isfinite (x2) );
+    assert (isfinite (roots.x1) );
+    assert (isfinite (roots.x2) );
 
-    printf ("\nProgram answer: \n\n");
+    printf ("\nProgram answer: \n\n");    //
 
-    switch (nRoots)
-        {
-            case NO_ROOTS:  printf ("There is no real roots :(");
-                            break;
+    switch (roots.nRoots)
+    {
+        case NO_ROOTS  : printf ("There is no real roots :(");
+                         break;
 
-            case ONE_ROOT:  printf ("The only root is %lg.", x1);
-                            break;
+        case ONE_ROOT  : printf ("The only root is %lg.", roots.x1);
+                         break;
 
-            case TWO_ROOTS: printf ("Roots are %lg and %lg.", x1, x2);
-                            break;
+        case TWO_ROOTS : printf ("Roots are %lg and %lg.", roots.x1, roots.x2);
+                         break;
 
-            case INF_ROOTS: printf ("Any root.");
-                            break;
+        case INF_ROOTS : printf ("Any root.");
+                         break;
 
-            default: printf ("Error");
-                     break;
-        }
+        default        : printf ("Error");
+                         break;
+    }
 }
 
 //-----------------------------------------------------------------------
@@ -327,73 +400,8 @@ void CleanBuffer ()
 {
     char ch = '\0';
 
-    while ( ch = getchar () != '\n' && ch != EOF) {}
-}
-
-//-----------------------------------------------------------------------
-
-void AllUnitsTester ()
-{
-    UnitSample test1 = {1, 2, -3, -3, 1, TWO_ROOTS, 1};
-    UnitSample test2 = {1, -5, 4, 5, 1, TWO_ROOTS, 2};
-    UnitSample test3 = {1, -2, 1, 1, 1, ONE_ROOT, 3};
-    UnitSample test4 = {1, 25, 0, -25, 0, ONE_ROOT, 4};
-    UnitSample test5 = {0, -2, 1, 0.5, 0, ONE_ROOT, 5};
-    UnitSample test6 = {3, 4, 5, 0, 0, NO_ROOTS, 6};
-    UnitSample test7 = {1, 2.5, -1.5, -3, 0.5, TWO_ROOTS, 7};
-    UnitSample test8 = {0, 0, 3, 0, 0, NO_ROOTS, 8};
-    UnitSample test9 = {0, 0, 0, 0, 0, INF_ROOTS, 9};
-
-
-    int counter = 0;
-
-    counter += UnitTester (UnitSample (test1) );
-    counter += UnitTester (UnitSample (test2) );
-    counter += UnitTester (UnitSample (test3) );
-    counter += UnitTester (UnitSample (test4) );
-    counter += UnitTester (UnitSample (test5) );
-    counter += UnitTester (UnitSample (test6) );
-    counter += UnitTester (UnitSample (test7) );
-    counter += UnitTester (UnitSample (test8) );
-    counter += UnitTester (UnitSample (test9) );
-
-    printf("-----------------------------------"
-           "Total: %d correct, %d incorrect.\n"
-           "-----------------------------------", counter, 9 - counter);
-}
-
-//------------------------------------------------------------------------------
-
-int UnitTester(struct UnitSample test)
-{
-    assert (isfinite (test.a) );
-    assert (isfinite (test.b) );
-    assert (isfinite (test.c) );
-    assert (isfinite (test.x1_exp) );
-    assert (isfinite (test.x2_exp) );
-
-    double x1 = 0;
-    double x2 = 0;
-
-    int nRoots = Dispatcher (test.a, test.b, test.c, &x1, &x2, ModeState);
-
-
-
-    if (nRoots != test.nRoots_exp || CompareTwo (x1, test.x1_exp) == false || CompareTwo (x2, test.x2_exp) == false)
+    while (ch = getchar () != '\n' && ch != EOF)
     {
-        printf ("%s----------\n"
-               "|Test #%d |: Incorrect execution:%s a = %lg, b = %lg, c = %lg,\n"
-                "%s----------%s                            x1 = %lg, x2 = %lg, nRoots = %d\n\n"
-                "               Expected: x1 = %lg, x2 = %lg, nRoots = %d.\n\n",
-                RED, test.TestNumber, RESET, test.a, test.b, test.c, RED, RESET, x1, x2,
-                nRoots, test.x1_exp, test.x2_exp, test.nRoots_exp);
-        return 0;
-    }
-    else
-    {
-        printf ("%s--------- \n"
-                "|Test #%d | : Correct execution.\n"
-                "--------- %s\n\n", GREEN, test.TestNumber, RESET);
-        return 1;
+
     }
 }
